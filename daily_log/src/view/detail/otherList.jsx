fbb02f5b8bc7f43ec02b950e12acb686ae08de6c
ssook1222 <chlthr1222@naver.com>
2023-03-03 12:08:54 +0900

    UPDATE : 필터 버전일 때 모바일 UI 수정

--- daily_log/src/view/detail/otherList.jsx
+++ daily_log/src/view/detail/otherList.jsx
@@ -1,32 +1,52 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useLocation} from 'react-router-dom';
 
 import Grid2 from "@mui/material/Unstable_Grid2";
-import {Card} from "@mui/material";
+import {Button, Card} from "@mui/material";
 
 import totalData from "../../data/totalData.json"
-import koreanData from "../../data/totalKoreanData.json"
-import JapanData from "../../data/totalJapaneseData.json"
+import data from "../../data/topData.json";
 
 function OtherList() {
-    let id = useLocation().state.data.id;
 
+    const [page, setPage] = useState(1);
+    const [start, setStart] = useState(0);
+    const [end, setEnd] = useState(3);
+    const pageNumber = [];
+    for (let i = 1; i < Math.ceil(data.list?.length / 3); i++) {
+        pageNumber.push(i);
+    }
+    useEffect(() => {
+        setStart((page - 1) * 3);
+        setEnd(page * 3);
+    }, [page]);
+
+    let id = useLocation().state.data.id;
     const newList = totalData.list.filter((data) => data.id !== id)
 
     return (
         <Grid2 container spacing={3} id="main-item-list">
                             <>
-                                {newList?.slice(0,3).map((contents) =>(
+                                {newList?.slice(start,end).map((contents) =>(
                                 <Grid2 item xs={4} sm={4} md={4}>
                                     {
                                         contents.id!==id &&
-                                        <Card className="back_list" style={{display: 'inline-block'}}>
+                                        <Card className="back_list" style={{display: 'inline-block', width:"100%"}}>
                                             <img className="list_image" src={contents.url}></img>
                                             <h2 className="list_card_title">{contents.name}</h2>
                                         </Card>
                                     }
                                 </Grid2>
                                 ))}
+                                <nav style={{ listStyleType: "none", display: "flex", width:"100%", marginBottom:"3%" }}>
+                                <div className={"list"} style={{ listStyleType: "none", display: "flex" }}>
+                                {pageNumber.map((num) => (
+                                  <li key={num} onClick={() => setPage(num)} style={{marginLeft:"auto", marginRight:"auto"}}>
+                                    <Button style={{marginRight:"20px"}} className={"page_but"} variant={"outlined"} size={"medium"}>{num}</Button>
+                                  </li>
+                                ))}
+                                </div>
+                              </nav>
                             </>
         </Grid2>
 
