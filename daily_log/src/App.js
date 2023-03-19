919e72b7077b94fcdae5725cdd562bbd347bf386
ssook1222 <chlthr1222@naver.com>
2023-03-19 20:26:43 +0900

    UPDATE : 음식점 검색바 UI 생성

--- daily_log/src/App.js
+++ daily_log/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 //routing pages
-import Main from "./view/main/main"
+import Main from "./view/start/start"
+import Top from "./view/main/main"
 import Total from "./view/total/total"
 import Detail from "./view/detail/detail"
 
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route>
             <Route exact path="/" element={<Main />}></Route>
+            <Route exact path="/top" element={<Top />}></Route>
             <Route exact path="/total" element={<Total />}></Route>
             <Route exact path="/detail" element={<Detail />}></Route>
         </Route>
