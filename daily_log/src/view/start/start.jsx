bfc657a3a979c19179205b0884145f7f3a15e843
ssook1222 <chlthr1222@naver.com>
2023-05-18 22:38:43 +0900

    UPDATE : 하단 버튼 UI 수정 및 검색 기능 구현

--- daily_log/src/view/start/start.jsx
+++ daily_log/src/view/start/start.jsx
@@ -3,38 +3,58 @@ import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import Button from "@mui/material/Button";
+import ReactSearchBox from "react-search-box";
 
-import { blueGrey } from '@mui/material/colors';
+import './start.css'
+import data from '../../data/totalData.json'
+
+import {useState} from "react";
 
 export default function CustomizedInputBase() {
 
+  const [search, setSearch] = useState('');
+
+  const onChange = (value) => {
+      if(value === "덕수궁 부대찌개" || value === "조조칼국수"){
+          console.log(value)
+          setSearch(value)
+      }
+  }
+
   return (
     <div style={{display:"flex", alignItems: "center", height:"100%", flexWrap: "wrap", justifyContent: "center"}}>
-        <h1 id="main-title">식당-일지</h1>
+        <h1 style={{ position: "fixed", marginTop:"-100px"}} id="main-title" >식당-일지</h1>
         <Paper
           component="form"
-          style={{marginRight:"auto", marginLeft:"auto", marginTop:"-700px", marginBottom:"-500px"}}
+          style={{marginRight:"auto", marginLeft:"auto", marginTop:"150px", position: "fixed", backgroundColor:'none'}}
           sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "90%" }}
         >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="음식점을 검색하세요."
-            inputProps={{ 'aria-label': '음식점을 검색하세요.' }}
-          />
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
+        <div style={{margin:"auto", display:"block", width:"100%"}}>
+              <ReactSearchBox
+                  placeholder="음식점을 입력해주세요."
+                  data={data.list}
+                  callback={record => console.log(record)}
+                  onFocus={() => {
+                    console.log('This function is called when is focussed')
+                  }}
+                  onChange={onChange}
+                  fuseConfigs={{
+                      threshold: 0.05,
+                  }}
+                  value=""
+              />
+          </div>
         </Paper>
 
-        <Button style={{width:"45%", textAlign:"center", marginTop:"-3%", background: "white", border: "1px solid grey"}}
-                    variant="outlined" component="label" >
-                <h3 style={{color: "gray"}}>추천 음식점</h3>
-        </Button>
-        <Button style={{width:"45%", textAlign:"center", marginTop:"-3%", marginLeft: "1%", background: "white", border: "1px solid grey"}}
-                    variant="outlined" component="label">
-                <h3 style={{color: "gray"}}>전체 음식점 보기</h3>
-        </Button>
+        <div style={{width: "100%", textAlign: 'center', marginTop: "300px",position:"fixed"}}>
+            <a href="#" style={{width: "30%", textDecorationLine: 'none', display: 'inline-block' }}>
+                    <h3 className="link-it" style={{color: "gray", fontSize: '15px'}}>추천 음식점 </h3>
+            </a>
+            | <div style={{display: 'inline-block'}}></div>
+            <a href="#" style={{width: "30%", textDecorationLine: 'none', display: 'inline-block'}}>
+                    <h3 className="link-it" style={{color: "gray", fontSize: '15px'}}>전체 음식점 보기</h3>
+            </a>
+        </div>
     </div>
   );
 }
